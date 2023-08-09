7cf8c49422d92d461f1c6a98f294e8b9b8331e77
송지훈 <jihoon.song@medisolution.co.kr>
2023-08-09 14:25:51 +0900

    코틀린 타입 명시 공부

--- src/main/kotlin/StudyKotlin.kt
+++ src/main/kotlin/StudyKotlin.kt
@@ -1,5 +1,7 @@
 // 코틀린 놀이터 : https://play.kotlinlang.org/
 
-fun main(){
-    println("Hello Kotlin!!");
+fun main() {
+    val a = readLine()!!.toInt()
+    val b: Int = readLine()!!.toInt()
+    println(a + b)
 }
