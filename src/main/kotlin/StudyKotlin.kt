a4ec07e09459d7156ebf33751ff75e439d112a37
송지훈 <jihoon.song@medisolution.co.kr>
2023-08-11 10:23:56 +0900

    코틀린 정수타입 상수 확인

--- src/main/kotlin/StudyKotlin.kt
+++ src/main/kotlin/StudyKotlin.kt
@@ -1,7 +1,14 @@
 // 코틀린 놀이터 : https://play.kotlinlang.org/
 
 fun main() {
-    val a = readLine()!!.toInt()
-    val b: Int = readLine()!!.toInt()
-    println(a + b)
+//    val a = readLine()!!.toInt()
+//    val b: Int = readLine()!!.toInt()
+//    println(a + b)
+
+    println(Float.MIN_VALUE)   // 1.4E-45
+    println(Double.MAX_VALUE)   // 1.797693134862315710308
+    println(Double.POSITIVE_INFINITY)   // Infinity
+    println(1.0 / Double.NEGATIVE_INFINITY)   // - 0.0
+    println(2 - Double.POSITIVE_INFINITY)   // -Infinity
+    println(3 * Float.NaN)   // NaN
 }
